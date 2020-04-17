ad692fc6cc0f0f66b88b8bf5bf29aa2bb1cfcf5e
MrShanu <63833049+MrShanu@users.noreply.github.com>
2020-04-17 09:25:19 +0500

    Update and rename zeroboy.py to Shanu.py

--- Shanu.py
+++ Shanu.py
@@ -49,17 +49,17 @@ def jalan(z):
 ##### LOGO #####
 logo = """ -----------------------------•◈•
 (  __)\\ ____--------------_------------•◈•
-|__(~)    •||•THE - AAHIL -OFFICAL------•◈•
-|__\~~) •||•RANA - RAJPUT---------------•◈•
-|__(-----\  •◈•------BLACK-TIGER--------•◈•
+|__(~)    •||•MR - SHANU -OFFICAL------•◈•
+|__\~~) •||•SHANU - HEER---------------•◈•
+|__(-----\  •◈•------THE-LUCIFER--------•◈•
 |__~~~\ •◈•-----█-------⑦-------█------•◈•
 |__~~~\ •◈•-----█-------⑧-------█------•◈•
 |__~~~\ •◈•-----█-------⑥-------█------•◈•
 \033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;FlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1; Aahilrna4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92mMR SHANU
+\033[1;96mInstagram \033[1;93m: \033[1;ibrahim_daniyal
+\033[1;96mFacebook  \033[1;93m: \033[1; Soldierspd
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/MrShanu
 \033[1;91m======================================="""
 
 def tik():
@@ -80,15 +80,15 @@ vuln = "\033[32mVuln"
 os.system("clear")
 print "\033[1;96m ============================================================="
 print  """\033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;92mFlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1;92m Aahilrana4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92mShanu CH
+\033[1;96mInstagram \033[1;93m: \033[1;92mFlowSHANU
+\033[1;96mFacebook  \033[1;93m: \033[1;92m soldierspd
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/MrShanu
 \033[1;91m======================================="""
 print " \x1b[1;93m============================================================="
 
-CorrectUsername = "rana"
-CorrectPassword = "rana"
+CorrectUsername = "shanu"
+CorrectPassword = "S.H"
 
 loop = 'true'
 while (loop == 'true'):
